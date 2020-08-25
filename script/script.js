c1bff41ff80b86bfc8a548a5d351086cbe6a4968
Olga Borisenko <olyabor800@gmail.com>
2020-08-26 00:45:28 +0300

    feat: add moving square

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 'use strict';
-let main = document.querySelector('main');
+let body = document.querySelector('body');
 function DomElement (selector, height, width, bg, fontSize) {
   this.selector = selector; 
   this.height = height; 
@@ -13,11 +13,9 @@ DomElement.prototype.createElem = function () {
   if (this.selector[0] === '.') {
     newElem = document.createElement('div');
     newElem.classList.add(this.selector.slice(1));
-    newElem.textContent = prompt('Введите текст');
   } else if (this.selector[0] === '#') {
     newElem = document.createElement('p');
     newElem.id = this.selector.slice(1);
-    newElem.textContent = prompt('Введите текст');
   }
   newElem.style.height = this.height;
   newElem.style.width = this.width;
@@ -25,7 +23,36 @@ DomElement.prototype.createElem = function () {
   newElem.style.fontSize = this.fontSize;
   return newElem;
 };
-let elem1 = new DomElement('.block', '30px', '100%', 'grey', '20px');
-main.append(elem1.createElem());
-let elem2 = new DomElement('#block', '30px', '50%', 'yellow', '16px');
-main.append(elem2.createElem());
+
+function draw (x, y) {
+  this.style.left = 10 * x + 'px';
+  this.style.top = 10 * y + 'px';
+}
+
+let square = new DomElement('#block', '100px', '100px', 'lime').createElem();
+square.style.position = 'absolute';
+body.append(square);
+let x = 0, y = 0;
+window.onkeydown = (event) => {
+  switch (event.key) {
+    case 'ArrowLeft':
+      x--;
+      draw.apply(square, [x, y]);
+      break;
+
+    case 'ArrowRight':
+      x++;
+      draw.apply(square, [x, y]);
+      break;
+
+    case 'ArrowDown':
+      y++;
+      draw.apply(square, [x, y]);
+      break;
+
+    case 'ArrowUp':
+      y--;
+      draw.apply(square, [x, y]);
+      break;
+  }
+};
