42f8b5652342b12bc2e858b4e9244f37436399cd
Olga Borisenko <olyabor800@gmail.com>
2020-08-26 00:10:03 +0300

    feat: add DomElement creating

---
+++ script/script.js
@@ -0,0 +1,31 @@
+'use strict';
+let main = document.querySelector('main');
+function DomElement (selector, height, width, bg, fontSize) {
+  this.selector = selector; 
+  this.height = height; 
+  this.width = width; 
+  this.bg = bg; 
+  this.fontSize = fontSize;
+}
+
+DomElement.prototype.createElem = function () {
+  let newElem;
+  if (this.selector[0] === '.') {
+    newElem = document.createElement('div');
+    newElem.classList.add(this.selector.slice(1));
+    newElem.textContent = prompt('Введите текст');
+  } else if (this.selector[0] === '#') {
+    newElem = document.createElement('p');
+    newElem.id = this.selector.slice(1);
+    newElem.textContent = prompt('Введите текст');
+  }
+  newElem.style.height = this.height;
+  newElem.style.width = this.width;
+  newElem.style.background = this.bg;
+  newElem.style.fontSize = this.fontSize;
+  return newElem;
+};
+let elem1 = new DomElement('.block', '30px', '100%', 'grey', '20px');
+main.append(elem1.createElem());
+let elem2 = new DomElement('#block', '30px', '50%', 'yellow', '16px');
+main.append(elem2.createElem());
